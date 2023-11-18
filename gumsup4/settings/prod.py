6acb3377025397caac3f14e380a4c88a9819d7f2
c browder <cbfif47@users.noreply.github.com>
2023-11-17 16:34:27 -0800

    Try putting static_url elsewhere

--- gumsup4/settings/prod.py
+++ gumsup4/settings/prod.py
@@ -8,9 +8,11 @@ DEBUG = False
 SECRET_KEY = os.environ['SECRET_KEY']
 ALLOWED_HOSTS = []
 
+
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+    STATIC_URL = os.environ.get('RENDER_EXTERNAL_HOSTNAME') + "/static/"
 
 ADMINS = []
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
