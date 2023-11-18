aa30d849c280d3d7f20889e63e2a66b5da69d540
c browder <cbfif47@users.noreply.github.com>
2023-11-17 16:46:36 -0800

    Following a tutoiral

--- gumsup4/settings/base.py
+++ gumsup4/settings/base.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'whitenoise.runserver_nostatic',  #add this line  
     "django.contrib.sites",
     "allauth",
     "allauth.account",
@@ -131,8 +132,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT  = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = os.path.join(BASE_DIR, "static")
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"#add this line
 
 SITE_ID = 1
