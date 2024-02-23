61f70ef309c8ef4e8ebc3af7dfd37dc68cc4dcf8
c browder <cbfif47@users.noreply.github.com>
2024-02-23 13:15:00 -0800

    Fixed counts

--- gumsup4/base/api/serializers.py
+++ gumsup4/base/api/serializers.py
@@ -19,14 +19,19 @@ class ItemFeedSerializer(ModelSerializer):
     user = UserSerializer()
     is_liked = serializers.BooleanField(default=False)
     is_saved = serializers.BooleanField(default=False)
-    likes_count = serializers.IntegerField()
-    comments_count = serializers.IntegerField()
+
+    def to_representation(self, instance):
+        """Convert `username` to lowercase."""
+        ret = super().to_representation(instance)
+        ret['likes_count'] = ItemLike.objects.filter(item=instance).count()
+        ret['comments_count'] = Comment.objects.filter(item=instance).count()
+        return ret
 
     class Meta:
         model = Item
         fields = ["id","name","author","note","item_type","rating"
         ,"status","started_date","ended_date","last_date","hide_from_feed"
-        ,"is_liked","is_saved","likes_count","comments_count","user"]
+        ,"is_liked","is_saved","user"]
 
     id = serializers.CharField(read_only=True)
     name = serializers.CharField(required=True,max_length=75)
@@ -39,6 +44,8 @@ class ItemFeedSerializer(ModelSerializer):
     ended_date = serializers.DateField(required=False)
     last_date = serializers.DateTimeField(read_only=True)
     hide_from_feed = serializers.BooleanField(default=False)
+
+
 
 
 class ItemSerializer(ModelSerializer):
