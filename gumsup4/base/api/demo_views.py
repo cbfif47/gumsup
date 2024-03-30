83a1a1e0deb6f921a4841cadea26776814b900d8
c browder <cbfif47@users.noreply.github.com>
2024-03-30 07:57:23 -0700

    fix indentation in folder parse

--- gumsup4/base/api/demo_views.py
+++ gumsup4/base/api/demo_views.py
@@ -20,39 +20,39 @@ from django.db.models import F
 
 def ParseFolders(folders):
 	for folder in folders:
-			id_index = folder.url.find("folders/") + 8
-			folder_id = folder.url[id_index:1000]
-			# need to do api key securley
-			parsed_url = "https://www.googleapis.com/drive/v3/files?fields=nextPageToken,files(id,name, createdTime, mimeType)&q='" + folder_id + "'+in+parents&key=" + settings.GOOGLE_DRIVE_KEY
-			r = requests.get(parsed_url)
-			files = r.json()["files"]
-			for f in files:
-				if f["mimeType"][0:5] == "audio":
-					file_url = "https://drive.google.com/uc?export=download&id=" + f["id"]
-					file_name = f["name"]
-					createdTime = f["createdTime"]
-					print(createdTime)
-					separator = file_name.find("-")
-					if separator > 0:
-						song_title = file_name[0:separator].strip()
-						version = file_name[(separator + 1):-4].strip()
-					else:
-						song_title = file_name[0:-4].strip()
-						version = "v1"
-					file_type = file_name[-3:]
-					# lets make songs if they dont exist, will default to no star or archive
-					song, song_created = DemoSong.objects.get_or_create(folder=folder,title=song_title)
-					if DemoDemo.objects.filter(song=song,url=file_url).exists() == False:
-						new_demo = DemoDemo.objects.create(song=song,url=file_url,version=version,source_created=createdTime,file_extension=file_type)
-			# now set priority
-			#songs = DemoSong.objects.filter(folder=folder)
-			demos = DemoDemo.objects.filter(song__folder=folder).annotate(row_number=Window(expression=RowNumber()
-				,partition_by=[F("song")]
-				,order_by=[F("source_created").desc()]))
-			for demo in demos:
-				demo.is_primary = (demo.row_number == 1) #all row 1s are primary TODO deal with manuals
-				demo.save()
-			return True
+		id_index = folder.url.find("folders/") + 8
+		folder_id = folder.url[id_index:1000]
+		# need to do api key securley
+		parsed_url = "https://www.googleapis.com/drive/v3/files?fields=nextPageToken,files(id,name, createdTime, mimeType)&q='" + folder_id + "'+in+parents&key=" + settings.GOOGLE_DRIVE_KEY
+		r = requests.get(parsed_url)
+		files = r.json()["files"]
+		for f in files:
+			if f["mimeType"][0:5] == "audio":
+				file_url = "https://drive.google.com/uc?export=download&id=" + f["id"]
+				file_name = f["name"]
+				createdTime = f["createdTime"]
+				print(createdTime)
+				separator = file_name.find("-")
+				if separator > 0:
+					song_title = file_name[0:separator].strip()
+					version = file_name[(separator + 1):-4].strip()
+				else:
+					song_title = file_name[0:-4].strip()
+					version = "v1"
+				file_type = file_name[-3:]
+				# lets make songs if they dont exist, will default to no star or archive
+				song, song_created = DemoSong.objects.get_or_create(folder=folder,title=song_title)
+				if DemoDemo.objects.filter(song=song,url=file_url).exists() == False:
+					new_demo = DemoDemo.objects.create(song=song,url=file_url,version=version,source_created=createdTime,file_extension=file_type)
+		# now set priority
+		#songs = DemoSong.objects.filter(folder=folder)
+		demos = DemoDemo.objects.filter(song__folder=folder).annotate(row_number=Window(expression=RowNumber()
+			,partition_by=[F("song")]
+			,order_by=[F("source_created").desc()]))
+		for demo in demos:
+			demo.is_primary = (demo.row_number == 1) #all row 1s are primary TODO deal with manuals
+			demo.save()
+	return True
 
 class MainView(APIView):
 	authentication_classes = [TokenAuthentication]
