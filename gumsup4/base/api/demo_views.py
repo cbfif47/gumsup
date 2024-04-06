c4920a0bef87a33c4c7a6663979475e8836c1ec3
c browder <cbfif47@users.noreply.github.com>
2024-04-06 11:19:57 -0700

    try removing slashes

--- gumsup4/base/api/demo_views.py
+++ gumsup4/base/api/demo_views.py
@@ -226,7 +226,7 @@ def AppleSiteAssociationView(request):
 				             "appIDs": [ "SN4RBG3Z5J.rexwithfriends.demoitis" ],
 				             "components": [
 				               {
-				                  "/": "/demoitis/sharelink/"
+				                  "/": "demoitis/sharelink"
 				               }
 				             ]
 				           }
