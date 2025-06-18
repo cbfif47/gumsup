1192e7c0c5ccede56e57eb6da1dd46d935d05874
c browder <cbfif47@users.noreply.github.com>
2025-06-17 19:40:15 -0700

    prod allowed hosts

--- gumsup4/settings/prod.py
+++ gumsup4/settings/prod.py
@@ -6,7 +6,9 @@ from .base import *
 
 DEBUG = False
 SECRET_KEY = os.environ['SECRET_KEY']
-ALLOWED_HOSTS = ['.rexwithfriends.com']
+ALLOWED_HOSTS = ['.rexwithfriends.com',
+    "thisismansions.com",
+    "www.thisismansions.com",]
 
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
